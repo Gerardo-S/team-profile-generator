7a7de995dbde313c6b6e063b72b7867d0f4a5be2
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-10-06 18:06:06 -0700

    generate html successful

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-function initManager(){
+const arrayTeam = [];
+initManager();
+
+// manager prompt to add team members----------------------------------------------------------------------------
+function initManager() {
     inquirer.prompt([
         {
             type: "input",
@@ -40,29 +44,32 @@ function initManager(){
                 "Engineer",
                 "Intern",
                 "I do not want to add any more team members."
-                
+
             ]
         }
 
-    ]).then(function(managerInput){
-        console.log(managerInput);
-        const manager = new Manager(managerInput.name,managerInput.id,managerInput.email,managerInput.officeNumber);
-        console.log(manager);
-        if (managerInput.teamMember == "Engineer"){
+    ]).then(function (managerInput) {
+        const manager = new Manager(managerInput.name, managerInput.id, managerInput.email, managerInput.officeNumber);
+        arrayTeam.push(manager);
+        if (managerInput.teamMember == "Engineer") {
             addEngineer();
         }
-        else if(managerInput.teamMember == "Intern"){
+        else if (managerInput.teamMember == "Intern") {
             addIntern();
 
+        } else {
+            const Html = render(arrayTeam)
+            writeToFile(Html);
         }
-        else{
-            render(manager);
-        }
-    });
-};
-initManager();
 
-function addEngineer(){
+    });
+
+};
+
+// Manager section ends here --------------------------------------------------------------------------------------------
+
+// If an Engineer is to be added to team information questions begin here------------------------------------------------
+function addEngineer() {
     inquirer.prompt([
         {
             type: "input",
@@ -95,23 +102,27 @@ function addEngineer(){
             ]
         }
 
-    ]).then(function(engineerInfo){
-        console.log(engineerInfo);
+    ]).then(function (engineerInfo) {
+        const engineer = new Engineer(engineerInfo.name, engineerInfo.id, engineerInfo.email, engineerInfo.github);
+        arrayTeam.push(engineer);
 
-        if (engineerInfo.teamMember == "Engineer"){
+        if (engineerInfo.teamMember == "Engineer") {
             addEngineer();
         }
-        else if(engineerInfo.teamMember == "Intern"){
+        else if (engineerInfo.teamMember == "Intern") {
             addIntern();
 
         }
-        else{
-            render();
+        else {
+            const Html = render(arrayTeam)
+            writeToFile(Html);
         }
     });
 };
+// Engineer section ends here--------------------------------------------------------------------------------------------
 
-function addIntern(){
+// If intern is chosen information questions are asked here--------------------------------------------------------------
+function addIntern() {
     inquirer.prompt([
         {
             type: "input",
@@ -144,40 +155,34 @@ function addIntern(){
             ]
         }
 
-    ]).then(function(internInfo){
-        console.log(internInfo);
+    ]).then(function (internInfo) {
+        const intern = new Intern(internInfo.name, internInfo.id, internInfo.email, internInfo.school);
+        arrayTeam.push(intern);
 
-        if (internInfo.teamMember == "Engineer"){
+        if (internInfo.teamMember == "Engineer") {
             addEngineer();
         }
-        else if(internInfo.teamMember == "Intern"){
+        else if (internInfo.teamMember == "Intern") {
             addIntern();
 
         }
-        else{
-            render();
+        else {
+            const Html = render(arrayTeam)
+            writeToFile(Html);
         }
     });
 };
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// Intern section ends here -----------------------------------------------------------------------------------------------
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+// function to transfer Html string to a new file--------------------------------------------------------------------------
+function writeToFile(Html){
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+    fs.writeFile(outputPath, Html, (err) => {
+        if(err){
+            console.log(err)
+            return console.log(err);
+        }
+        console.log("success")
+    });
+};
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
